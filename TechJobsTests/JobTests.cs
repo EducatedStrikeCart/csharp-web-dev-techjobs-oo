4fcd586d4fe3da1ce24351cf149ad8efa8212729
EducatedStrikeCart <dhn004@gmail.com>
2021-10-21 20:38:04 -0400

    refactored tests to be more scalable

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -8,52 +8,58 @@ namespace TechJobsTests
     [TestClass]
     public class JobTests
     {
+        Job defaultTestJob;
+        [TestInitialize]
+        public void InitDefaultTestJob()
+        {
+            defaultTestJob = new("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        }
+
         [TestMethod]
-        public void A0_TestSettingJobId() //This test must be run first
+        public void TestJobIdBeginsWithOne() //This test must be run first
+        {
+            Assert.AreEqual(2, defaultTestJob.Id); //This value is 2 because Id is static and defaultTestJob is initialized
+        }
+
+        [TestMethod]
+        public void TestSettingJobId()
         {
 
             Job testJob1 = new();
-            Job testJob2 = new();
 
-            Assert.AreEqual(1, testJob1.Id);
-            Assert.AreEqual(2, testJob2.Id);
+            Assert.IsTrue(defaultTestJob.Id + 1 == testJob1.Id);
         }
 
         [TestMethod]
         public void TestJobConstructorSetsAllFields()
         {
-            Job testJob = new("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-
-            Assert.AreEqual("Product Tester", testJob.Name);
-            Assert.AreEqual("ACME", testJob.EmployerName.Value);
-            Assert.AreEqual("Desert", testJob.EmployerLocation.Value);
-            Assert.AreEqual("Quality control", testJob.JobType.Value);
-            Assert.AreEqual("Persistence", testJob.JobCoreCompetency.Value);
+            Assert.AreEqual("Product Tester", defaultTestJob.Name);
+            Assert.AreEqual("ACME", defaultTestJob.EmployerName.Value);
+            Assert.AreEqual("Desert", defaultTestJob.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", defaultTestJob.JobType.Value);
+            Assert.AreEqual("Persistence", defaultTestJob.JobCoreCompetency.Value);
         }
 
         [TestMethod]
         public void TestJobsForEquality()
         {
-            Job testJob = new("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
             Job testJob1 = new("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
-            Assert.AreEqual(4, testJob.Id);
-            Assert.AreEqual(5, testJob1.Id);
+            Assert.IsFalse(testJob1.Id == defaultTestJob.Id);
         }
 
         [TestMethod]
         public void TestJobsToString()
         {
-            Job testJob = new("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
             string correctResult =
-                "ID: 6\n" +
+                "ID: 5\n" +
                 "Name: Product Tester\n" +
                 "Employer: ACME\n" +
                 "Location: Desert\n" +
                 "Position Type: Quality control\n" +
                 "Core Competency: Persistence";
 
-            string testResult = testJob.ToString();
+            string testResult = defaultTestJob.ToString();
 
             Assert.AreEqual(correctResult, testResult);
         }
@@ -68,11 +74,11 @@ namespace TechJobsTests
             Assert.AreEqual("OOPS! This job does not seem to exist.", testResult);
         }
 
-             
+
         [TestMethod]
         public void TestJobsToString_Blank()
         {
-            Job testJob = new("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency("")); ;
+            Job testJob = new("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
             string correctResult =
                 "ID: 7\n" +
                 "Name: Data not available\n" +
